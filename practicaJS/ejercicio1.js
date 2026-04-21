0da6adeb5819f84ab4b0ea7b7e0dae7b209134b4
tatii0801 <tatiananieva0801@gmail.com>
2026-04-21 10:28:29 -0300

    Se creó la carpeta practicaJS para almacenar los distintos archivos .js, incluyendo ejercicio1.js.

---
+++ practicaJS/ejercicio1.js
@@ -0,0 +1,7 @@
+const integrantes = "Agustin Pablo Portillo, Josías Daniel Rodríguez, Moisés Antonio Israel Flores Navajas, Tatiana Valeria Nieva, Yesarela Febe Manuelita Flores Navajas";
+
+const mostrarMensaje = () => {
+    console.log(`Somos ${integrantes} y estamos aprendiendo JavaScript.`);
+};
+
+mostrarMensaje();
